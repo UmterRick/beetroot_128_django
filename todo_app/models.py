d7312ccffbc39a75c0dbd4011026096a240bcd2a
umterrick <usichenko.vlad67@gmail.com>
2025-01-19 11:50:36 +0200

    Lesson 42

--- todo_app/models.py
+++ todo_app/models.py
@@ -11,3 +11,7 @@ class Task(models.Model):
     priority_level = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)], default=3)
     deadline = models.DateTimeField()
     created_at = models.DateTimeField(auto_now=True)
+
+    @classmethod
+    def above_priority_level(cls, level):
+        return cls.objects.filter(priority_level__gt=level)
--- todo_app/urls.py
+++ todo_app/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from todo_app.views import TaskListView, TaskCreateView, TaskDetailsView, TaskEditView, TaskDeleteView
 
 urlpatterns = [
-    path('', TaskListView.as_view(), name="task_list"),
-    path('create/', TaskCreateView.as_view(), name="task_create"),
-    path('<int:pk>', TaskDetailsView.as_view(), name="task_details"),
-    path('<int:pk>/update/', TaskEditView.as_view(), name="task_edit"),
-    path('<int:pk>/delete/', TaskDeleteView.as_view(), name="task_delete"),
+    path('', TaskListView.as_view(), name="task-list-new"),
+    path('create/', TaskCreateView.as_view(), name="task-create"),
+    path('<int:pk>', TaskDetailsView.as_view(), name="task-details"),
+    path('<int:pk>/update/', TaskEditView.as_view(), name="task-edit"),
+    path('<int:pk>/delete/', TaskDeleteView.as_view(), name="task-delete"),
 ]
